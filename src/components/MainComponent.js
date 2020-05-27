4bc0e6287cb49673e31040e204ab663418266c9e
Abhinav Kumar <sahabhinav36@gmail.com>
2020-05-27 22:43:21 +0530

    Single Page Application 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent'
+import Contact from './ContactComponent';
 import  {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions'; 
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Footer from './FooterComponent'
 class Main extends Component {
@@ -11,11 +15,17 @@ class Main extends Component {
     super(props);
         this.state={
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
         }
     }
     render(){
         const HomePage = () => {
-            return <Home/>;
+            return <Home dish={this.state.dishes.find(dish => dish.featured)}        
+            promotion={this.state.promotions.find(promo => promo.featured)}
+            leader={this.state.leaders.find(leader => leader.featured)}
+            />;
         }
         return (
         <div>
@@ -23,6 +33,7 @@ class Main extends Component {
             <Switch>
                 <Route path="/home" component={HomePage} />
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                <Route exact path="/contactus" component={Contact} />
                 <Redirect to="/"/>
             </Switch>
             <Footer/>
